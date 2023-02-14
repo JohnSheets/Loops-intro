5442d5db671d38a87901661025b1494ebbdf606b
John Sheets <johnsheets410@gmail.com>
2023-02-14 14:29:33 -0500

    Loop exercises completed becasue I am a rockstar

---
+++ main.js
@@ -0,0 +1,11 @@
+var cohortOneStudents = ["Sable", "David", "Joey", "Nick", "Tommy", "Connor", "Charles", "Russ", "Bobby", "Josh", "Nikki", "Matt", "Dale", "Sydney"]
+
+for(var i = 0; i < cohortOneStudents.length; i++){
+    console.log(cohortOneStudents[i]);
+  };
+
+  myIntrests = ['fishing', 'hiking', 'videogames', 'coding', 'riding'];
+  for (var i = 0; i < myIntrests.length; i++) {
+    console.log('one of my intrests is:' + myIntrests[i]);
+  };
+  
